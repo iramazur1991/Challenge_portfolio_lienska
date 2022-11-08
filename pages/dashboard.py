0a03bc89535f02ac3817d183e32fd10aa2190169
Iryna Lienska <iramazur1991@gmail.com>
2022-11-08 20:41:55 +0000

    homework2

---
+++ pages/dashboard.py
@@ -0,0 +1,27 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage)
+    button_xPath = "//*[@id='login']"
+//div/div[@class='MuiCardMedia-root jss145']
+//a/span[text()='Dev team contact']
+//h2[text()='Scouts Panel']
+//div[text()='Matches count']
+//span[text() ='Main page']
+//span[text() ='Players']
+//h2[text()='Activity']
+//div[text()='Events count']
+//p[contains(@class,'body2')]
+
+
+ADD A MATCH FORM
+//div/input[@name='email']
+//div/input[@name='phone']
+//div[@aria-haspopup='listbox'][contains(@id,'leg')]
+//font[text()='Facebook']
+//font[text()='Надіслати']
+//button[contains(@class,'containedSecondary')]
+//font[text()='Вийти з аккаунта']
+//input[@name='age']
+//font[text()='Додайте посилання на Youtube']
+//div[@class='MuiCardHeader-root']
