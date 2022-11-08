802d3db2e8488b5a7d2b97d18ff36f365f7e3a0c
Iryna Lienska <iramazur1991@gmail.com>
2022-11-08 20:47:20 +0000

    commit#1 HW2

---
+++ pages/add a match form.py
@@ -0,0 +1,14 @@
+from pages.base_page import BasePage
+
+
+
+//div/input[@name='email']
+//div/input[@name='phone']
+//div[@aria-haspopup='listbox'][contains(@id,'leg')]
+//font[text()='Facebook']
+//font[text()='Надіслати']
+//button[contains(@class,'containedSecondary')]
+//font[text()='Вийти з аккаунта']
+//input[@name='age']
+//font[text()='Додайте посилання на Youtube']
+//div[@class='MuiCardHeader-root']
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -12,16 +12,3 @@ class Dashboard(BasePage)
 //h2[text()='Activity']
 //div[text()='Events count']
 //p[contains(@class,'body2')]
-
-
-ADD A MATCH FORM
-//div/input[@name='email']
-//div/input[@name='phone']
-//div[@aria-haspopup='listbox'][contains(@id,'leg')]
-//font[text()='Facebook']
-//font[text()='Надіслати']
-//button[contains(@class,'containedSecondary')]
-//font[text()='Вийти з аккаунта']
-//input[@name='age']
-//font[text()='Додайте посилання на Youtube']
-//div[@class='MuiCardHeader-root']
